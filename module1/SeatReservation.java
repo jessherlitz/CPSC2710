612f00df6eb53f71448200b14067f430b9729d63
Jessica Sampaio-Herlitz <102844641+jessampaio@users.noreply.github.com>
2024-03-07 17:46:38 -0500

    added methods

--- module1/SeatReservation.java
+++ module1/SeatReservation.java
@@ -1,7 +1,51 @@
 public class SeatReservation {
-	
+
 	private String flightDesignator
 	private java.time.LocalDate flightDate
 	private String firstName
 	private String lastName
+
+	public void setFlightDesignator(String fd) {
+        this.flightDesignator = fd;
+    }
+
+    public LocalDate getFlightDate() {
+        return flightDate;
+    }
+
+    public void setFlightDate(LocalDate date) {
+        this.flightDate = date;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fn) {
+        this.firstName = fn;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String ln) {
+        this.lastName = ln;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("SeatReservation{");
+        sb.append("flightDesignator=");
+        sb.append(flightDesignator != null ? flightDesignator : "null");
+        sb.append(",flightDate=");
+        sb.append(flightDate != null ? flightDate.toString() : "null");
+        sb.append(",firstName=");
+        sb.append(firstName != null ? firstName : "null");
+        sb.append(",lastName=");
+        sb.append(lastName != null ? lastName : "null");
+        sb.append("}");
+        return sb.toString();
+    }
 }
